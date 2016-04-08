45ea7643edc1109d605505654dd97b8a4dc61afe
saurabh <saurabh.1e1@gmail.com>
2016-04-08 17:39:44 +0530

    token access security

--- src/utils/api.py
+++ src/utils/api.py
@@ -1,5 +1,7 @@
-from flask_restful import Resource, Api
 from functools import wraps
+from flask_restful import Resource, Api
+from flask_security import auth_token_required, roles_required
+
 from .blue_prints import bp
 
 api = Api(bp)
@@ -13,4 +15,4 @@ def authenticate(func):
 
 
 class BaseResource(Resource):
-    method_decorators = [authenticate]
+    method_decorators = [auth_token_required, roles_required('admin')]
