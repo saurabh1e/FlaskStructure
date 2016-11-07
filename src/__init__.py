5291e2c6d994863b7962b07a9fab8b8580405c56
saurabh_1e <saurabh.1e1@gmail.com>
2016-11-07 12:37:06 +0530

    admin panel bug

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,4 @@ from .utils import api, db, ma, create_app, ReprMixin, BaseResource, bp, BaseMix
 
 from .user import apiv1, models, schemas
 from .utils.security import security
+from .admin_panel import admin_manager
